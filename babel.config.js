a9ae6e593ebcfe8747bef25c93aef31373837558
Anthonin Clara <anthonin.clara@epitech.eu>
2022-05-09 14:41:34 +0200

    feat: folders and empty index

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            utils: './src/utils',
+            assets: './assets',
+            types: './src/types',
+            components: './src/components',
+            constants: './src/constants',
+            hooks: './src/hooks',
+            providers: './src/providers',
+            navigation: './src/navigation',
+            screens: './src/screens',
+            themes: './src/themes',
+          },
+          root: ['./src'],
+        },
+      ],
+    ],
   };
 };
