8b6f07f5a6098ead5d10b436250d59af49ec6d90
Lucas Fabreguettes <lucas31ff@gmail.com>
2022-05-24 16:50:01 +0200

    feat: add firebase authentication

--- app/src/components/forms/register/RegisterForm.tsx
+++ app/src/components/forms/register/RegisterForm.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {ViewRow} from 'components/layouts/FlexLayout/FlexViews';
-import {FormInput} from "components/common/FormInput/FormInput";
-import {CheckBox} from "components/common/CheckBox/CheckBox";
-import {ClassicButton} from "components/common/ClassicButton/ClassicButton";
-import useTheme from 'hooks/useTheme';
+import React from "react";
+import { Text, View } from "react-native";
+import { ViewRow } from "components/layouts/FlexLayout/FlexViews";
+import { FormInput } from "components/common/FormInput/FormInput";
+import { CheckBox } from "components/common/CheckBox/CheckBox";
+import { ClassicButton } from "components/common/ClassicButton/ClassicButton";
+import useTheme from "hooks/useTheme";
 import useRegisterForm from "./useRegisterForm";
-import fnStyles from './RegisterFormStyle';
-import {JText} from "components/common/Text/Text";
-import {useTranslation} from 'react-i18next';
+import fnStyles from "./RegisterFormStyle";
+import { JText } from "components/common/Text/Text";
+import { useTranslation } from "react-i18next";
 
 type Props = {
-  onSubmit: (username: string, email: string, password: string) => Promise<void>;
+  onSubmit: (
+    username: string,
+    email: string,
+    password: string
+  ) => Promise<void>;
   style?: object;
 };
 
@@ -27,38 +31,68 @@ const RegisterForm = (props: Props) => {
     setConfirmPassword,
     termsAgreed,
     onTermsCheckboxClicked,
-    buttonEnabled
+    buttonEnabled,
   } = useRegisterForm(props.onSubmit);
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   const styles = fnStyles(theme);
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <View>
-      <JText style={styles.title} labelKey={"auth.setUpAccount"}/>
-      <JText style={styles.subtitle} labelKey={"auth.setUpAccountExplanation"}/>
-      <View style={{height: 40}}/>
-      <FormInput value={username} onChange={setUsername} placeholderKey={"auth.username"} style={styles.input}/>
-      <FormInput value={email} onChange={setEmail} placeholderKey={"auth.email"} style={styles.input}/>
-      <FormInput value={password} onChange={setPassword} placeholderKey={"auth.password"} style={styles.input} secure/>
-      <FormInput value={confirmPassword} onChange={setConfirmPassword} placeholderKey={"auth.confirmPassword"}
-                 style={styles.input} secure/>
-      <ViewRow align={"center"} style={{marginVertical: theme.sizings.sz25}}>
-        <CheckBox value={termsAgreed} onPress={onTermsCheckboxClicked} style={{marginRight: 15}}/>
+      <JText style={styles.title} labelKey={"auth.setUpAccount"} />
+      <JText
+        style={styles.subtitle}
+        labelKey={"auth.setUpAccountExplanation"}
+      />
+      <View style={{ height: 40 }} />
+      <FormInput
+        value={username}
+        onChange={setUsername}
+        placeholderKey={"auth.username"}
+        style={styles.input}
+      />
+      <FormInput
+        value={email}
+        onChange={setEmail}
+        placeholderKey={"auth.email"}
+        style={styles.input}
+      />
+      <FormInput
+        value={password}
+        onChange={setPassword}
+        placeholderKey={"auth.password"}
+        style={styles.input}
+        secure
+      />
+      <FormInput
+        value={confirmPassword}
+        onChange={setConfirmPassword}
+        placeholderKey={"auth.confirmPassword"}
+        style={styles.input}
+        secure
+      />
+      <ViewRow align={"center"} style={{ marginVertical: theme.sizings.sz25 }}>
+        <CheckBox
+          value={termsAgreed}
+          onPress={onTermsCheckboxClicked}
+          style={{ marginRight: 15 }}
+        />
         <Text style={styles.termsLabel}>
-          {t('terms.agreeWith')}
-          <Text style={styles.termsLinks}>
-            {t('terms.terms')}
-          </Text>
-          {t('terms.and')}
-          <Text style={styles.termsLinks}>
-            {t('terms.privacyPolicy')}
-          </Text>
-          {t('terms.pleaseAgree')}
+          {t("terms.agreeWith")}
+          <Text style={styles.termsLinks}>{t("terms.terms")}</Text>
+          {t("terms.and")}
+          <Text style={styles.termsLinks}>{t("terms.privacyPolicy")}</Text>
+          {t("terms.pleaseAgree")}
         </Text>
       </ViewRow>
-      <ClassicButton onPress={() => {
-      }} labelKey={"auth.register"} type={'classic'} enabled={buttonEnabled}/>
+      <ClassicButton
+        onPress={() => {
+          props.onSubmit(username, email, password);
+        }}
+        labelKey={"auth.register"}
+        type={"classic"}
+        enabled={buttonEnabled}
+      />
     </View>
   );
 };
--- app/src/screens/auth/register/RegisterScreen.tsx
+++ app/src/screens/auth/register/RegisterScreen.tsx
@@ -1,37 +1,47 @@
-import React, {useEffect} from 'react';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from 'navigation/RootStackParamList';
+import React, { useEffect } from "react";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "navigation/RootStackParamList";
 import { AuthLayout } from "components/layouts/AuthLayout/AuthLayout";
-import {Divider} from "components/common/Divider/Divider";
-import {JText} from "components/common/Text/Text";
-import {TextButton} from "components/common/TextButton/TextButton";
-import fnStyles from './RegisterScreenStyle';
+import { Divider } from "components/common/Divider/Divider";
+import { JText } from "components/common/Text/Text";
+import { TextButton } from "components/common/TextButton/TextButton";
+import fnStyles from "./RegisterScreenStyle";
 import useTheme from "hooks/useTheme";
-import {View} from "react-native";
-import {ViewRow} from "components/layouts/FlexLayout/FlexViews";
+import { View } from "react-native";
+import { ViewRow } from "components/layouts/FlexLayout/FlexViews";
 import RegisterForm from "components/forms/register/RegisterForm";
 import useAlert from "providers/AlertProvider";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 import useAuth from "hooks/useAuth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../firebase";
 
-export type ScreenProps = NativeStackScreenProps<RootStackParamList, 'register'>;
+export type ScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  "register"
+>;
 
 const RegisterScreen = ({ navigation }: ScreenProps) => {
-  const {theme} = useTheme();
+  const { theme } = useTheme();
   const styles = fnStyles(theme);
   const { setAlert } = useAlert();
   const { t } = useTranslation();
   const { register, isRegisterError: isFetchingError } = useAuth();
 
   const handleAlreadyHaveAnAccount = () => {
-    navigation.navigate('login');
-  }
+    navigation.navigate("login");
+  };
 
-  const handleRegister = async (username: string, email: string, password: string) => {
-    const isUserLogged = await register(username, email, password);
-
-    if (isUserLogged === true || (typeof isUserLogged === 'object' && 'id' in isUserLogged)) {
-      // navigation.navigate('Root');
+  const handleRegister = async (
+    username: string,
+    email: string,
+    password: string
+  ) => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      navigation.navigate("Root");
+    } catch (error) {
+      alert(error.message);
     }
   };
 
@@ -39,25 +49,35 @@ const RegisterScreen = ({ navigation }: ScreenProps) => {
     if (isFetchingError) {
       setAlert({
         color: theme.colors.statusDangerHigh,
-        message: t('alert.error'),
-        title: t('alertTitle.error'),
+        message: t("alert.error"),
+        title: t("alertTitle.error"),
       });
     }
   }, [isFetchingError, setAlert, t, theme]);
 
   return (
     <AuthLayout>
-      <View style={{
-        flex: 1,
-        width: "65%",
-        paddingTop: 50,
-      }}>
+      <View
+        style={{
+          flex: 1,
+          width: "65%",
+          paddingTop: 50,
+        }}
+      >
         <RegisterForm onSubmit={handleRegister} />
-        <View style={{flex: 1}}>
-          <Divider style={styles.divider}/>
+        <View style={{ flex: 1 }}>
+          <Divider style={styles.divider} />
           <ViewRow align={"center"}>
-            <JText style={styles.navigation} labelKey={"auth.alreadyHaveAnAccount"}/>
-            <TextButton onPress={handleAlreadyHaveAnAccount} labelKey={"auth.signin"} fontSizeName={"large"} isBold={true}/>
+            <JText
+              style={styles.navigation}
+              labelKey={"auth.alreadyHaveAnAccount"}
+            />
+            <TextButton
+              onPress={handleAlreadyHaveAnAccount}
+              labelKey={"auth.signin"}
+              fontSizeName={"large"}
+              isBold={true}
+            />
           </ViewRow>
         </View>
       </View>
