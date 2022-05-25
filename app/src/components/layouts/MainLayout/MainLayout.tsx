f001ca1db369635664a633984424d5c0c3b95f5c
Anthonin Clara <anthonin.clara@epitech.eu>
2022-05-25 23:22:12 +0200

    feat: current team layout

--- app/src/components/layouts/MainLayout/MainLayout.tsx
+++ app/src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {Image, View, Text} from 'react-native';
+import {Image, View, Text, TouchableOpacity} from 'react-native';
 import {ViewCol, ViewRow} from '../FlexLayout/FlexViews';
 import {User} from "types/User";
 import {Team} from "types/Team";
 import {Channel} from "../../../types/Channel";
 import {Friend} from "../../../types/Friend";
 import {TeamLayout} from "./TeamLayout";
+import {Avatar} from "../../common/Avatar/Avatar";
+import {CurrentTeamLayout} from "./CurrentTeamLayout";
 
 // TODO Pass the team
 type Props = {
@@ -34,15 +36,15 @@ export const MainLayout = (props: Props) => {
       <ViewRow align={"center"} justify={"space-between"} style={{backgroundColor: '#000', height: '5%', minHeight: 55, width: '100%', paddingHorizontal: 20, paddingVertical: 10}}>
         <View/>
         <Text style={{color: '#FFF', fontWeight: "600"}}>Club Manchot | Team | Channel</Text>
-        <View style={{width: 35, height: 35, borderRadius: 35/3, backgroundColor: 'red'}}/>
+        <TouchableOpacity onPress={() => {}}>
+          <Avatar sizeName={"sz35"} />
+        </TouchableOpacity>
       </ViewRow>
       <ViewRow align={"center"} justify={"center"} style={{flex: 1}}>
-        <ViewRow style={{height: '100%', width: '17%', minWidth: 150, backgroundColor: '#171630'}}>
+        <ViewRow style={{height: '100%', width: '17%', minWidth: 200, backgroundColor: '#171630'}}>
           <TeamLayout onTeamClicked={props.onTeamClicked} teams={props.teams} currentTeam={props.currentTeam} />
           <ViewCol style={{height: '100%', borderWidth: 1, borderBottomWidth: 0, borderColor: '#393939'}}>
-            <ViewRow style={{height: 50, borderBottomWidth: 1, borderBottomColor: '#393939'}}>
-              <Text>Team !</Text>
-            </ViewRow>
+            <CurrentTeamLayout team={props.teams[props.currentTeam]}/>
             <Text>Channel</Text>
             <Text>Channel</Text>
           </ViewCol>
--- app/src/providers/ThemeProvider.tsx
+++ app/src/providers/ThemeProvider.tsx
@@ -32,6 +32,7 @@ export type Theme = {
     high: number;
     sz15: number;
     sz25: number;
+    sz35: number;
     sz50: number;
     sz60: number;
     sz95: number;
@@ -119,6 +120,7 @@ const getTheme = (width: number, height: number, scale: number): Theme => {
       high: 40,
       sz15: 15,
       sz25: 25,
+      sz35: 35,
       sz50: 50,
       sz60: 60,
       sz95: 95,
